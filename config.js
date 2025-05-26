9e34681992ac7a3daed48ba7f0d0cd835a4f236f
GamGam582 <guetebilyas92@gmail.com>
2025-05-26 15:31:09 +0100

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: "0l7iss.aternos.me",
-  port: 31911,
-  username: "Gamer" ,
-  actionDelay: 10000 // كل 10 ثواني يدير action
+  host: "0l7iss.aternos.me",     // ✏️ عدلها لو كانت عندك دومين مختلف
+  port: 31911,                   // ✏️ عدلها إذا تبدل البورت
+  username: "appah", // ✏️ سميتك اللي بغيتي، ما غاديش تتبدل
+  actionDelay: 10000             // delay بين كل action
 };
